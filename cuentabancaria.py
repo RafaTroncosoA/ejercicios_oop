c9411da41204fae0d54702b5c8cddb1ad26345d0
Rafael Troncoso <rafa.troncoso.a@gmail.com>
2022-10-30 20:11:08 -0300

    Parte 2 ejercicio cuenta-usuario

--- cuentabancaria.py
+++ cuentabancaria.py
@@ -1,3 +1,5 @@
+# CUENTA
+
 class CuentaBancaria:
     todas_las_cuentas=[]
 
@@ -31,12 +33,47 @@ class CuentaBancaria:
             print(j)
 
 
+# USUARIO
+class Usuario:
+    #Atributo de clase
+    nombre_banco='Primer Dojo Nacional'
+    lista_cuentas={}
+    # Atributos de instancia
+    def __init__(self,name,email):
+        self.name= name
+        self.email= email
+        self.cuenta = CuentaBancaria(tasa_interes=0.02, balance=0)	# añadió esta línea
+    # Métodos de instancia
+    def hacer_deposito(self,amount):
+        self.cuenta.deposito += amount
+        return self  # Permite repetir operaciones en la misma linea ej: rta.hacer_deposito(100).hacer_deposito(20).hacer_deposito(50)
+    
+    # hacer_retiro(self, amount): haz que este método reduzca el balance del usuario en la cantidad especificada 
+    def hacer_retiro(self,amount):
+        if self.cuenta.balance-amount>0:
+            self.cuenta.balance-=amount
+        else:
+            return 'Sin saldo suficiente'
+    #mostrar_balance_usuario(self): haz que este método imprima el nombre del usuario y el balance de cuenta en la terminal 
+    def mostrar_balance_usuario(self):
+        print('Usuario: '+self.name+' ,Balance:' +str(self.cuenta.balance))
 
-cuenta_1 = CuentaBancaria(0.1,100)
-cuenta_2 = CuentaBancaria(0.3,20)
+    # BONUS: transfer_dinero(self, other_user, amount): haz que este método reduzca el balance del usuario por el monto y agrega esa cantidad al balance de otro_usuario 
+    def transferencia_dinero(self,other_user,amount):
+        if type(other_user) is not Usuario:
+            print('Usuario no existe')
+        else:
+            if self.cuenta.balance-amount>0:
+                self.cuenta.retiro(amount)
+                other_user.cuenta.deposito(amount)
+            else:
+                print('Sin Saldo suficiente')
 
 
-cuenta_1.deposito(100).deposito(100).deposito(100).generar_interes().mostrar_info_cuenta()
-cuenta_2.deposito(100).deposito(100).retiro(150).retiro(10).generar_interes().mostrar_info_cuenta()
+RTA = Usuario('RTA','RTA@gma')
+GVE = Usuario('GVE','GVE@gma')
 
-CuentaBancaria.todas_las_cuentas
+RTA.cuenta.deposito(100).deposito(200).retiro(100).generar_interes().mostrar_info_cuenta()
+
+
+
